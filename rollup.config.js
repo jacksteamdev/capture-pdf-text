a8fb7ea1dc75285ba6601fc9690faaa64ff84da8
Jack Steam <teamsteamdev@gmail.com>
2017-12-14 11:21:52 -0600

    Refactor to reduce UMD build size

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
+import builtins from 'rollup-plugin-node-builtins'
 import babel from 'rollup-plugin-babel'
 import pkg from './package.json'
 
@@ -13,8 +14,11 @@ export default [
     },
     name: 'capturePDF',
     plugins: [
-      resolve(), // so Rollup can find `ms`
-      commonjs(), // so Rollup can convert `ms` to an ES module
+      resolve({
+        preferBuiltins: true
+      }), // so Rollup can find dependencies
+      builtins(), // so Rollup can include node global depencencies
+      commonjs(), // so Rollup can convert dependencies to an ES module
       babel({
         exclude: 'node_modules/**' // only transpile our source code
       })
@@ -22,14 +26,9 @@ export default [
   },
 
   // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
   {
     input: 'src/main.js',
-    external: ['ms'],
+    external: ['zlib', 'fs', 'http', 'https', 'url', 'pdfjs-dist'],
     output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' }
