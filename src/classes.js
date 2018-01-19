5c0eb80c1a0d65bc77d9a422b70cce2ad127a3e3
Jack Steam <jacksteamdev@gmail.com>
2018-01-19 09:47:35 -0600

    Add getters to Block class

--- src/classes.js
+++ src/classes.js
@@ -23,7 +23,62 @@ export class Item {
 }
 
 export class Block extends Array {
+  getRawText () {
+    return this.reduce((r, i) => r + i.text, '')
+  }
+
   get text () {
-    return this.reduce((r, s) => r + s.text, '')
+    return this.reduce((r, i, n) => `${r} ${i.text.trim()}`, '').trim()
+  }
+  set text (t) {
+    return undefined
+  }
+
+  // this.top = bottom + height
+  get top () {
+    return this.reduce((r, { top }) => Math.max(r, top), 0)
+  }
+  set top (n) {
+    return undefined
+  }
+
+  // this.right = left + width
+  get right () {
+    return this.reduce((r, { right }) => Math.max(r, right), 0)
+  }
+  set right (n) {
+    return undefined
+  }
+
+  // this.bottom = bottom
+  get bottom () {
+    return this.reduce((r, { bottom }) => Math.min(r, bottom), Infinity)
+  }
+  set bottom (n) {
+    return undefined
+  }
+
+  // this.left = left
+  get left () {
+    return this.reduce((r, { left }) => Math.min(r, left), Infinity)
+  }
+  set left (n) {
+    return undefined
+  }
+
+  // Add getters for dimensions
+  get height () {
+    return this.top - this.bottom
+  }
+  set height (n) {
+    return undefined
+  }
+
+  // this.width = width
+  get width () {
+    return this.right - this.left
+  }
+  set width (n) {
+    return undefined
   }
 }
