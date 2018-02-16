f473b94bb0e31110539268aa031bcaa2b190bfb8
Jack Steam <teamsteamdev@gmail.com>
2018-02-16 16:47:49 -0600

    Rename matcher-recursive to matcher.recursive

--- tests/matcher-recursive.test.js
+++
@@ -1,9 +0,0 @@
-//TODO: matcher-rules
-
-recurseIntoBlocks
-
-items = multi paragraph
-partial = recurseIntoBlocks(rules)
-expect(partial).toBeInstanceOf(Function)
-result = partial(items)
-expect(result).toBeInstanceOf(Array)
