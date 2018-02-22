437b7bea64f6199551e5ad8951185aef40729f49
Jack Steam <teamsteamdev@gmail.com>
2018-02-22 15:43:25 -0600

    Add flattenBlocks

--- src/main.js
+++ src/main.js
@@ -1,27 +1,25 @@
 import { groupBy } from './matcher'
-import { Block } from './class.block'
 import { sameStyle, sameLine, sameBlock } from './rules'
 import {
-  validCharBlock,
+  flattenBlocks,
+  invalidCharBlock,
   emptyItem,
-  mapListItem,
+  toListItem,
 } from './main.utils'
 
 import flow from 'lodash/fp/flow'
-import reject from 'lodash/fp/reject'
-import get from 'lodash/fp/get'
 import map from 'lodash/fp/map'
+import reject from 'lodash/fp/reject'
 
 /**
  * parseTextItems :: [item] -> [block]
  */
 export const parseTextItems = flow(
   groupBy(sameStyle()),
-  reject(validCharBlock),
-  Block.from,
-  get('items'),
+  reject(invalidCharBlock),
+  flattenBlocks,
   groupBy(sameLine()),
   reject(emptyItem),
-  map(mapListItem),
+  map(toListItem),
   groupBy(sameBlock()),
 )
--- src/main.utils.js
+++ src/main.utils.js
@@ -1,8 +1,10 @@
 import { Block } from './class.block'
 
+import flow from 'lodash/fp/flow'
+import get from 'lodash/fp/get'
 import uniq from 'lodash/fp/uniq'
 
-export const validCharBlock = ({ text }) => {
+export const invalidCharBlock = ({ text }) => {
   const chars = uniq([...text])
     .join('')
     .replace(' ', '')
@@ -23,10 +25,12 @@ export const isListItem = ({ items: [first, second] }) =>
   first.right < second.left &&
   isListHead(first.text)
 
-export const mapListItem = block => {
+export const toListItem = block => {
   if (isListItem(block)) {
     return Block.from(block, { listItem: block.items[0] })
   } else {
     return block
   }
 }
+
+export const flattenBlocks = flow(Block.from, get('items'))
